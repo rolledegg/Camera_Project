ab930ef9eeddec17020e9a3b035917148465ed71
username <wkadlf2310@gmail.com>
2022-03-07 23:56:36 +0900

    2022 / 03 / 07

---
+++ bmi-calculator/app/src/main/java/smu/app/myapplication/ResultActivity.kt
@@ -0,0 +1,27 @@
+package smu.app.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.math.pow
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        //getIntent 없이 바로 Intent 사용
+        val height = intent.getIntExtra("height",0)
+        val weight = intent.getIntExtra("weight",0)
+
+        val bmi = weight / (height / 100.0).pow(2.0)
+        val resultText = when{
+            bmi >= 35.0 -> "고도 비만"
+            bmi >= 30.0 -> "중정도 비만"
+            bmi >= 25.0 -> "경도 비만"
+            bmi >= 23.0 -> "과체중"
+            bmi >= 18.5 -> "정상 체중"
+            else -> "저체중"
+        }
+
+    }
+}
