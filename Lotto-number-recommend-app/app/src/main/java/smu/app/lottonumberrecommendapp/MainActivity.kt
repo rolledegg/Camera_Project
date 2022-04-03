032df570b00b07f42e7a203bfe37177598a69046
username <wkadlf2310@gmail.com>
2022-04-03 23:57:17 +0900

    04/03

--- Lotto-number-recommend-app/app/src/main/java/smu/app/lottonumberrecommendapp/MainActivity.kt
+++ Lotto-number-recommend-app/app/src/main/java/smu/app/lottonumberrecommendapp/MainActivity.kt
@@ -2,8 +2,20 @@ package smu.app.lottonumberrecommendapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val  clearButton: Button by lazy {
+        findViewById<Button>(R.id.clearButton)
+    }
+    private val  addButton: Button by lazy {
+        findViewById<Button>(R.id.addButton)
+    }   
+    private val  runButton: Button by lazy {
+        findViewById<Button>(R.id.runButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
